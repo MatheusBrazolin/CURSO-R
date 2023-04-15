b39ac1c22a4bdec8507b2dedf22d306933bce92e
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2023-04-15 11:39:47 -0300

    Add files via upload

---
+++ Aula 1.R
@@ -0,0 +1,76 @@
+setwd("C:/Users/profslvo.ANIMAEDU/Documents/Linguagem R - Matheus Brazolin")
+getwd()
+
+#===============================================================================
+
+# 52+8 #ADIÇÃO 
+# 52-2 #SUBITRAÇÃO 
+# 5%%3 #RESTO DA DIVISÃO 
+# 6%/%3 #DIVISÃO INTEIRA
+
+#===============================================================================
+
+#VARIAVEL
+# a=42
+# b="nome"
+# c<-"a"
+# d<-"a"
+
+# NOME= "MATHEUS FELICIO BRAZOLIN"
+# numero= 19
+# peso<- 65
+# sexo<- 'M'
+# 
+# class(NOME)
+# mode(numero)
+# typeof(peso)
+
+#===============================================================================
+
+# dir()
+# ls()
+
+#===============================================================================
+
+# as.character(50)
+# as.integer('500')
+# as.numeric(60)
+
+#===============================================================================
+
+# vetores_caracteres <- c("Matheus", "Felicio", "Brazolin")
+# vetor_numero <- c(41,8,79,63,54)
+# vetor_numero[-3]
+# vetor_palavra <- c("joao", "Maria", "Matheus")
+# vetor_palavra
+# vetor_logico <- c(TRUE, FALSE, T,F)
+# vetor_logico
+     
+#===============================================================================
+
+
+vetor_numero <- c(41,8,79,63,54)
+vetor_numero2  <- c(41,8,79,36, 46)
+
+vetor_numero+vetor_numero2
+
+vetor1 <- c(1,2,3,4,5)
+vetor2 <- c(6,7,8)
+
+
+vetor1+vetor2
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
