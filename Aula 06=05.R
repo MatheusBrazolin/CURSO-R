3447028ff2340fb6c40adb069f01cc1e90a9a798
Matheus Brazolin <102265721+MatheusBrazolin@users.noreply.github.com>
2023-05-06 11:42:04 -0300

    Aula06=05

---
+++ Aula 06=05.R
@@ -0,0 +1,162 @@
+search()
+
+install.packages("ggvis")
+library(ggvis)
+
+
+install.packages(c("dplyr", "tidy"))
+library("dplyr")
+library("tidy")
+
+install.packages("ggplot2")
+library("ggplot2")
+
+str('allpackage:ggplot2' <-
+      install.packages(.Library,priority = "high"))
+allPackages
+
+======================================================
+
+if (% < 30 ){
+  "numero menorque 30"
+}
+
+if (x<7){
+  "numero menor que 7"
+}else{
+    "esse numero não é menor que 7"
+}
+
+if (x<7){
+  "numero menor que 7"
+}else if (x==7){
+  "Numero 7"
+}else{
+  "esse numero não é menor que 7"
+}
+
+======================================================
+
+x=3
+ifelse(x%%2==0,"par", "impar")
+
+x+1
+repeat{
+  x=x+3
+  if(x>99){
+  break
+  }
+  print(x)
+}
+
+for (i in 1:20){
+  print(i)
+}
+for (i in morm(10)) {
+  print(i)
+}
+ffor (i in 1:22) {
+  if(i !=13 || i!=15){
+    print(i)
+  }
+}
+
+======================================================
+  
+#Exercicio do 1 ao 50 
+  
+  for (i in 1:50){
+    print(i)
+  }
+    
+  x=-1
+  while (x<50) {
+    x=x+1
+    print(x)
+  }
+  
+  
+  args(mean)
+  }
+  sample(x=c(1:50), size=10)
+
+
+======================================================|
+    
+    seq(100)
+  seq(C(1:100))
+
+
+  seq(500)
+  seq(C(1:500))
+
+
+======================================================
+
+
+Vetor <- C(1.5, 2.5, 8.4, 3.7, 6.3)
+
+  Vetor <- sort(Vetor, decreasing = TRUE)
+  
+Vetor <- append (vetor, 9.6)
+vetor
+
+
+
+======================================================
+
+
+  func1 <1 function(x){
+    x+x
+  }
+
+func2 <- function (a,b){
+  valor+ a^b
+return(valor)
+
+}
+
+======================================================
+
+Vetor1 <-10;13
+Vetor2 <- c("a","b","c", "d")
+Vetor3 <- c(6.5,9.2,11.9,5.1)
+
+
+funcao <-  function(....){
+  df = data.frame(cbind(...))
+  return(df)
+  
+}
+  
+  df1 <- funcao (vetor1)
+  df2 <- funcao (Vetor2, Vetor3)
+  df3 <- funcao (Vetor1, Vetor2, Vetor3)
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
